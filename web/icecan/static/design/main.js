3024ab47a076c5fa6427f485bb7f8bdda84799df
tomage <tomas@tomasarni.net>
2011-05-30 22:21:19 +0000

    Made extra panels more generic. Added annotations.

--- web/icecan/static/design/main.js
+++ web/icecan/static/design/main.js
@@ -1,15 +1,15 @@
 
-
-
-$(function () {
+var extra_selectors = new Array('.old', '.annotations');
 
 function displayExtra(event) {
   var t = $(this);
-  $('.old:visible').hide();
-  var old = t.find('.old');
-  old.css('top', -t.height());
-  old.css('float', 'left');
-  old.show();
+  $.each(extra_selectors, function (idx, extra_selector)
+    {
+      $(extra_selector+':visible').hide();
+      var els = t.find(extra_selector);
+      els.css('top', -t.height());
+      els.show();
+    });
 }
 
 function toggleUserPanel(event) {
@@ -18,8 +18,8 @@ function toggleUserPanel(event) {
   return false;
 }
   
+
+$(function () {
   $('#user #hide').bind('click', toggleUserPanel);
-  
   $('.article').hover(displayExtra);
-  
 });

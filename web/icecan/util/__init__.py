4166a5647468bd935d5b9404e4f2f5c4e703c0f3
tomage <tomas@tomasarni.net>
2011-07-04 21:44:11 +0000

    Adding slugify util function (works for icelandic letters)

--- web/icecan/util/__init__.py
+++ web/icecan/util/__init__.py
@@ -1,3 +1,6 @@
+#! -*- coding: utf-8 -*-
+
+import re
 
 class Response(dict):
     def __init__(self, request=None):
@@ -18,3 +21,34 @@ def add_response(view):
         kwargs.update({'response': r})
         return view(request, *args, **kwargs)
     return responsed_view
+
+def slugify(input):
+    output = input.strip('_.- ').lower()
+    # Letters    
+    output = output.replace(u'á', 'a')
+    output = output.replace(u'ð', 'd')
+    output = output.replace(u'é', 'e')
+    output = output.replace(u'í', 'i')
+    output = output.replace(u'ó', 'o')
+    output = output.replace(u'ú', 'u')
+    output = output.replace(u'ý', 'y')
+    output = output.replace(u'þ', 'th')
+    output = output.replace(u'æ', 'ae')
+    output = output.replace(u'ö', 'o')
+    # text
+    output = output.replace('&', 'og') 
+    output = output.replace('/', '_')
+    # commas
+    output = output.replace(',', '_')
+
+    output = output.rstrip()
+    # special chars
+    output = re.sub('\s+', '_', output)
+    output = re.sub('[^\w.-]', '', output)
+
+    # squash underscores
+    while '__' in output:
+        output = output.replace('__', '_')
+
+    return output
+

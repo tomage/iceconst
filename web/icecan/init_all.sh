d69debc5c451fe17192f1eda1db92142e1325588
tomage <tomas@tomasarni.net>
2011-07-07 20:27:24 +0000

    Added generation of submodels for texts in init script

--- web/icecan/init_all.sh
+++ web/icecan/init_all.sh
@@ -1,1 +1,1 @@
-./manage.py reset main && ./manage.py loaddata main_dump.json && ./runserver.sh
+./manage.py reset main && ./manage.py loaddata main_dump.json && python main/text.py --generate-submodels-for-all-texts && ./runserver.sh
